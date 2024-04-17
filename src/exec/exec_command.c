663a02e28eba8090e2836105cfa3ee803baff122
LuisTakeo <luistakeo@hotmail.com>
2024-04-17 10:17:00 -0300

    Montagem de comando e execução atraves do comando fornecido de forma simples, sem validações

--- src/exec/exec_command.c
+++ src/exec/exec_command.c
@@ -12,21 +12,28 @@
 
 #include "../includes/minishell.h"
 
-void	exec_command(char **arrstr, int id, char **env)
+void	exec_command(char **arrstr, int id, char **env, char **path)
 {
-	int i;
+	int 	i;
+	char	*full_path;
 
 	i = 0;
+	full_path = ft_strjoin(path[6], arrstr[0]);
+	ft_printf("%s\n", full_path);
+
 	id = fork();
 	if (id)
 	{
 		waitpid(id, &i, 0);
 		ft_printf("Response: %d\n", i);
+		free(full_path);
 	}
 	if (!id)
 	{
+
 		ft_printf("Processo filho: %d\n", id);
 		// execve("/usr/bin/bash", arrstr, env);
-		execve("/usr/bin/ls", arrstr, env);
+		execve(full_path, arrstr, env);
+		// execve("/usr/bin/clear", arrstr, env);
 	}
 }
