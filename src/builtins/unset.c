a6cf2f1fe091d1275286224930de8d9000892562
Drielly De Fatima Sanches Da Silva <dde-fati@c1r4p4.42sp.org.br>
2024-04-28 18:00:27 -0300

    upt: pwd atualizada com ft_fdprintf

--- src/builtins/unset.c
+++ src/builtins/unset.c
@@ -1,22 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pwd.c                                              :+:      :+:    :+:   */
+/*   unset.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dde-fati <dde-fati@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/25 20:06:13 by dde-fati          #+#    #+#             */
-/*   Updated: 2024/04/28 17:59:49 by dde-fati         ###   ########.fr       */
+/*   Created: 2024/04/28 17:58:22 by dde-fati          #+#    #+#             */
+/*   Updated: 2024/04/28 17:58:30 by dde-fati         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
-
-void	pwd(void)
-{
-	char	*pwd;
-
-	pwd = getcwd(NULL, 0);
-	ft_fdprintf("%s\n", STDOUT_FILENO, pwd);
-	free(pwd);
-}
