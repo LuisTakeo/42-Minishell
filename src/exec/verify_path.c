53ee397a542a522f177780800a48940ccd29f315
LuisTakeo <luistakeo@hotmail.com>
2024-05-03 10:39:39 -0300

    Update: atualizado prompt com correçõess para segfault

--- src/exec/verify_path.c
+++ src/exec/verify_path.c
@@ -14,13 +14,11 @@
 
 char	*verify_path(char *bin, char **path)
 {
-	int		response;
 	int		i;
 	char	*full_path;
 
 	if (access(bin, F_OK) == 0 && access(bin, X_OK) == 0)
 		return (bin);
-	response = 0;
 	i = 0;
 	while (path[i])
 	{
